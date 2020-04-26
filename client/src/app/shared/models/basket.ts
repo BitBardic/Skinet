ec657114f34d5711bbfb6ae838b4ce53bbb97022
Nasir Iqbal <nasirpucit@hotmail.com>
2020-04-26 18:23:16 +0100

    S14. Added basket features to angular app

---
+++ client/src/app/shared/models/basket.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+export interface IBasket {
+  id: string;
+  items: IBasketItem[];
+  // getBasketQuantity: () => number;
+}
+
+export interface IBasketItem {
+  id: number;
+  productName: string;
+  price: number;
+  quantity: number;
+  pictureUrl: string;
+  brand: string;
+  type: string;
+}
+
+export class Basket implements IBasket {
+  id = uuidv4();
+  items: IBasketItem[] = [];
+//   getBasketQuantity() {
+//     let quantity = 0;
+//     for (const item of this.items) {
+//       quantity += item.quantity;
+//     }
+//     return quantity;
+//   }
+}
+
+export interface IBasketTotals {
+  shipping: number;
+  subtotal: number;
+  total: number;
+}
