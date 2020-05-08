ef7d8215a0cd82d0fc7837bcfc80a0968da807af
Nasir Iqbal <nasirpucit@hotmail.com>
2020-05-08 19:42:57 +0100

    S19. Implemented client checkout using multi-step wizard and auto send JwtToken using HttpInterceptor

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -5,6 +5,7 @@ import { ReplaySubject, of } from 'rxjs';
 import { IUser } from '../shared/models/user';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { IAddress } from '../shared/models/address';
 
 @Injectable({
   providedIn: 'root',
@@ -21,10 +22,13 @@ export class AccountService {
       this.currentUserSource.next(null);
       return of(null);
     }
-    let headers = new HttpHeaders();
-    headers = headers.set('Authorization', `Bearer ${token}`);
 
-    return this.http.get(this.baseUrl + 'account', {headers}).pipe(
+    //  Commenting out below as we've setup our token in jwt.interceptor.ts
+    // let headers = new HttpHeaders();
+    // headers = headers.set('Authorization', `Bearer ${token}`);
+    // return this.http.get(this.baseUrl + 'account', {headers}).pipe(
+
+    return this.http.get(this.baseUrl + 'account').pipe(
       map((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
@@ -64,5 +68,13 @@ export class AccountService {
 
   checkEmailExists(email: string) {
     return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
   }
 }
